58068046096f862abf0dcf1798bcf0240904ad39
jwb1220 <798099726@qq.com>
2020-07-08 13:13:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import app from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      msg: '',
+      itemList: [],
+      delItemList: []
+    }
+    this.handleAddClick = this.handleAddClick.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleDelItemClick = this.handleDelItemClick.bind(this)
+    this.handleEnter = this.handleEnter.bind(this)
+    this.handleRemoveItem = this.handleRemoveItem.bind(this)
+  }
+
+  render() {
+    return (
+      <div className="hello">
+        <h3>TodoList</h3>
+        <div className="w100 flexcc">
+          <div className="minh900 w100 flexcc col">
+
+            {/* input */}
+            <div className="mt20 flexcc">
+              <input onChange={this.handleInputChange} onKeyUp={this.handleEnter} value={this.state.msg} placeholder="Todo" type="text" className="mr20" />
+              <button onClick={this.handleAddClick} >add</button>
+            </div>
+
+            <div className="flexfs jcsa w50 mt40 ml50">
+
+              {/* 待办 */}
+              <div className={app.minw200}>
+                {
+                  this.state.itemList.map((item, index) => {
+                    return (
+                      <div key={index} className="mt20 fs12 flexc w100">
+                        <div className="mr20">待完成</div>
+                        <div onClick={this.handleDelItemClick} className={[app.shou, 'flexcc', 'shenhui'].join(' ')}>{item}</div>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+
+              {/* 已完成 */}
+              <div className={['minh900', 'ml50', app.minw200].join(' ')}>
+                {
+                  this.state.delItemList.map((item, index) => {
+                    return (
+                      <div key={index} className={['flexcfs', app.shou, 'fs12', 'shenhui', 'w100', 'mt20'].join(' ')}>
+                        <div className="mr20">已完成</div>
+                        <div onClick={this.handleRemoveItem} className={app.huadiao}>{item}</div>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+
+            </div>
+
+          </div>
+
+
+
+        </div>
+      </div>
+    )
+  }
+  componentDidMount() {  // 生命周期
+    this.getData()
+  }
+
+  getData() {  // 从 localStorage 获取缓存的数据
+    this.setState({
+      itemList: JSON.parse(window.localStorage.getItem('itemList')) ? JSON.parse(window.localStorage.getItem('itemList')) : [],
+      delItemList: JSON.parse(window.localStorage.getItem('delItemList')) ? JSON.parse(window.localStorage.getItem('delItemList')) : []
+    })
+  }
+  handleInputChange(e) {  // input 双向同步
+    this.setState({
+      msg: e.target.value
+    })
+  }
+  handleAddClick() {  // add
+    let itemList2 = this.state.itemList
+    itemList2.push(this.state.msg)
+    this.setState({
+      itemList: itemList2,
+      msg: ''
+    })
+    window.localStorage.setItem('itemList', JSON.stringify(this.state.itemList))
+  }
+  handleEnter(e) {  // enter
+    if (e.keyCode !== 13) return
+    this.handleAddClick()
+  }
+  handleDelItemClick(e) {  // delItem
+    let item = e.target.innerHTML.trim()
+    let index = this.state.itemList.findIndex(item2 => {
+      return item2 === item
+    })
+    let itemList2 = this.state.itemList
+    itemList2.splice(index, 1)
+    this.setState({
+      itemList: itemList2
+    })
+
+    let delItemList2 = this.state.delItemList
+    delItemList2.unshift(item)
+    this.setState({
+      delItemList: delItemList2
+    })
+
+    window.localStorage.setItem('itemList', JSON.stringify(this.state.itemList))
+    window.localStorage.setItem('delItemList', JSON.stringify(this.state.delItemList))
+  }
+  handleRemoveItem(e) {  // removeItem
+    let item = e.target.innerHTML.trim()
+    let index = this.state.delItemList.findIndex(item2 => {
+      return item2 === item
+    })
+    let delItemList2 = this.state.delItemList
+    delItemList2.splice(index, 1)
+    this.setState({
+      delItemList: delItemList2
+    })
+
+    let itemList2 = this.state.itemList
+    itemList2.unshift(item)
+    this.setState({
+      itemList: itemList2
+    })
+
+    window.localStorage.setItem('itemList', JSON.stringify(this.state.itemList))
+    window.localStorage.setItem('delItemList', JSON.stringify(this.state.delItemList))
+  }
+
 }
 
 export default App;
